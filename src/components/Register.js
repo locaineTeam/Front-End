d7f8e42405e40edc5decfaf5dd0e2e9e9dc9cb7c
Fabian Ramirez <fabian.ramirez-p@mail.escuelaing.edu.co>
2021-10-26 19:12:58 -0500

    Modificacion de Registro

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,5 +1,40 @@
+import { useState } from "react";
+
 export const Register = () => {
+	
+	const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [msj, setMsj] = useState("");
+	
+	const handleSubmit = (event) => {
+        event.preventDefault();
+
+        const data = {
+            email: email,
+            password: password
+        };
+
+    };
+	
+	const handleEmailChange = (e) => {
+        const value = e.target.value;
+        setEmail(value);
+    };
+
+    const handlePwdChange = (e) => {
+        const value = e.target.value;
+        setPassword(value);
+    };
+	
     return (
+		<>
         <h1>Registrarse</h1>
+        <form onSubmit={handleSubmit}>
+            <input type="text" placeholder="email" value={email} onChange={handleEmailChange}></input>
+            <input type="password" placeholder="password" value={password} onChange={handlePwdChange}></input>
+            <button>Registrarse</button>
+            <label>{msj}</label>
+        </form>
+        </>
     );
 }
