9dcbe418f60a435174dee5eb8747905942aea612
Verbo Camacho V <verbocamacho@gmail.com>
2021-10-28 15:38:33 -0500

    imagenes edit facade y perfil

--- src/components/EditFacade.js
+++ src/components/EditFacade.js
@@ -1,7 +1,9 @@
-
+import download from '../Assets/img/download.jpg';
 export const EditFacade = () => {
 
     return (
-        <h1>Edit Facade</h1>
+        
+        <><h1>Edit Facade</h1>
+        <img src={download} alt="description" /></>
     );
 }
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,7 +1,8 @@
-
+import downloade from '../Assets/img/Nicolle Figueroa.jpg';
 export const Profile = () => {
-
+    
     return (
-        <h1>Profile</h1>
+        <><h1>Profile</h1>
+        <img src={downloade} alt="description" /></>
     );
 }
