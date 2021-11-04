35b5d4a73368fd2807d46d12b111ebb7d394be24
KyrieLW <james.weisnner@mail.escuelaing.edu.co>
2021-11-03 23:15:16 -0500

    adding placeholder match window with university logos

--- src/components/Home.js
+++ src/components/Home.js
@@ -78,12 +78,12 @@ export const Home = () => {
                         </a>                
                     </div>
                    
-                    <div className="collapse navbar-collapse" id="navbarsHome">
+                    <div className="navbar" id="navbarsHome">
                         
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item dropdown">
                                 <a className="nav-link dropdown-toggle" href="#" id="Opciones" data-bs-toggle="dropdown" aria-expanded="false">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-person" viewBox="0 0 16 16">
                                     <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"/>
                                     </svg>
                                     Opciones
--- src/components/Match.js
+++ src/components/Match.js
@@ -1,7 +1,32 @@
+import React from 'react';
 
 export const Match = () => {
+    const cache = {};
+
+    function importAll(r) {
+        r.keys().forEach((key) => (cache[key] = r(key)));
+    }
+
+    importAll(require.context("../Assets/university_logo", false, /.(png|jpe?g|svg)$/));
+
+    const images = Object.entries(cache).map(module => module[1].default);
 
     return (
-        <h1>Match</h1>
+        <>
+        <p>Media Page..</p>
+
+        {Object.entries(cache).map(module => {
+            const image = module[1].default;
+            const name = module[0].replace("./","");
+            return (
+                <button className="btn btn-outline-secondary">
+                <div style={{float: 'left', padding: 100, margin: 10, border: '2px solid white' }}>
+                    <img style={{width: 100, height: 70, margin: 'auto', display: 'block'}} src={image} />
+                    <p>{name.replace('.png','')}</p>
+                </div>
+                </button>
+            )
+        })}
+    </>
     );
 }
