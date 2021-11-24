73426ae3660b6b5a358b12a79ac2b97306d4ab8a
Sim0no <german.marin@mail.escuelaing.edu.co>
2021-11-24 00:38:09 -0500

    Falta Mostrar bien universidades

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,49 @@
 import { HeaderContent } from "./HeaderContent";
 import { useHistory } from "react-router";
+import { variables } from '../providers/Variables';
+import $ from 'jquery';
+import React, { useEffect } from 'react';
+import { UniDiv } from "./UniversityDiv";
 
 export const Home = () => {
+    
+    
+
+    const getUniversitys = () =>{
+       
+        fetch(variables.LOCAL_URL+'v1/university')
+        .then(response=>response.json())
+        .then(resp=>{
+            $("#statTable tbody").empty();
+            //console.log(resp);
+            var contact = resp;
+            
+            console.log(contact);
+            if(contact !== undefined){
+                var data = contact.map((info) => {
+                    return {
+                        id: info.id,
+                        name: info.name
+                    }
+                })
+        
+
+                
+                data.map((info) => {
+                    var x = 0;
+                    $("#statTable > tbody:last").append($("<tr><td>" +  <UniDiv title={info.name}/>+
+                         
+                        "</td></tr>"))
+                })
+            } else {
+                
+            }})
+        ;
+
+    };
+
+
+
 
     const history = useHistory();
 
@@ -9,27 +51,42 @@ export const Home = () => {
         history.push("/university/eci");
     }
 
+
+    useEffect(()=>{
+
+        getUniversitys();  
+        
+        
+    }, []);
+
     return(
         <>
+        
         <HeaderContent/>
+        
         <section className="homepage py-3">
             <div className="subHomepage mx-auto p-2 rounded">
                 <h2>Universidades</h2>
                 <ul className="p-0">
-                    <li className="border rounded p-3 mb-2 li-uni">
-                        <div>
-                            <h5>Escuela Colombiana de Ingenieria</h5>
-                            <p>eci</p>
-                            <button className="btn btn-primary" onClick={handleUniversity}>Entrar</button>
-                        </div>
-                    </li>
-                    <li className="border rounded p-3 li-uni">
-                        <div>
-                            <h5>Escuela Colombiana de Ingenieria</h5>
-                            <p>eci</p>
-                            <button className="btn btn-primary">Entrar</button>
-                        </div>
-                    </li>
+                <table class="table table-hover table-borderless table-striped" id="statTable">
+                    <tbody>
+                                            
+                        <li className="border rounded p-3 mb-2 li-uni">
+                            <div>
+                                <h5>Escuela Colombiana de Ingenieria</h5>
+                                <p>eci</p>
+                                <button className="btn btn-primary" onClick={handleUniversity}>Entrar</button>
+                            </div>
+                        </li>
+                        <li className="border rounded p-3 li-uni">
+                            <div>
+                                <h5>Escuela Colombiana de Ingenieria</h5>
+                                <p>eci</p>
+                                <button className="btn btn-primary">Entrar</button>
+                            </div>
+                        </li>
+                    </tbody>
+                    </table>
                 </ul>
 
             </div>
---
+++ src/components/UniversityDiv.js
@@ -0,0 +1,26 @@
+import { useHistory } from "react-router";
+export const UniDiv = ({ title}) => {
+
+    const history = useHistory();
+    console.log(title);
+    console.log("mamahuevo");
+    const handleUniversity = () => {
+        history.push("/university/eci");
+    }
+    
+    var aux = [];
+    aux.push(<li className="border rounded p-3 mb-2 li-uni">
+    <div>
+       <h5>{title}</h5>
+            
+       <button className="btn btn-primary" onClick={handleUniversity}>Entrar</button>
+    </div>
+</li>);
+
+    return (
+        <div>
+            {aux}
+        </div>
+        
+    );
+}
