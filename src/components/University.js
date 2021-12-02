bf14acc498c260df478599f3166607dc2b056999
StivenVanegas <nevits28@hotmail.com>
2021-12-01 22:12:40 -0500

    edited chat group

--- src/components/University.js
+++ src/components/University.js
@@ -1,49 +1,40 @@
 import { HeaderContent } from "./HeaderContent";
 import { useState } from 'react';
-import { useParams } from "react-router";
+import { useParams, useHistory } from "react-router";
 import { useData } from "../providers/DataProvider";
 import SockJsClient from 'react-stomp';
 import { variables } from '../providers/Variables';
 
 export const University = () => {
 
+    const history = useHistory();
     const { data, setData } = useData();
     const user = data.user;
     const [clientRef, setClienteRef] = useState();
     const { universityId } = useParams();
-    const [usersOnline, setUsersOnline] = useState([]);
     const [messages, setMessages] = useState([]);
     const [message, setMessage] = useState("");
 
     const userDataDto = {
         id: user.id,
         name: user.name,
-        lastName: user.lastName
+        lastName: user.lastName,
+        message: message
     };
 
-
     const onMessageReceive = (msg , topic) => {
-        if(topic === "/topic/university/"+universityId){
-            console.log(msg);
-            setUsersOnline(msg);
-        }if(topic === "/topic/university/chat/"+universityId){
-            setMessages(msg);
+        if(topic === "/topic/university/chat/"+universityId){
+            const arr = [...messages];
+            arr.push(msg);
+            setMessages(arr);
         } else {
             console.log(msg);
         }
     }
 
-    const handleConnect = () => {
-        clientRef.sendMessage("/app/university/"+universityId, JSON.stringify(userDataDto));
-    }
-
-    const handleDisconnect = () => {
-        clientRef.sendMessage("/app/disconnect/"+universityId, JSON.stringify(userDataDto));
-    }
-
     const handleSendMsg = () => {
+        clientRef.sendMessage("/app/university/chat/"+universityId, JSON.stringify(userDataDto));
         setMessage("");
-        clientRef.sendMessage("/app/university/chat/"+universityId, message);
     }
 
     const handleChangeMsg = (e) => {
@@ -51,12 +42,15 @@ export const University = () => {
         setMessage(value);
     }
 
+    const handleClickName = (userId) => {
+        history.push("/profile/"+userId);
+    }
+
     return (
         <>
             <SockJsClient url={variables.API_URL + "stompendpoint"}
-                topics={["/topic/university/" + universityId, "/topic/disconnect/" + universityId, "/topic/university/chat/"+universityId]}
-                onMessage={onMessageReceive} ref={(client) => { setClienteRef(client) }}
-                onConnect={handleConnect} onDisconnect={handleDisconnect} />
+                topics={["/topic/university/chat/"+universityId]}
+                onMessage={onMessageReceive} ref={(client) => { setClienteRef(client) }} />
 
             <HeaderContent />
             <section className="uni-container py-3">
@@ -70,7 +64,10 @@ export const University = () => {
                             
                             {messages.map((msg) => {
                                 return(
-                                    <p className="chat-text border rounded p-1">{msg}</p>
+                                    <div className="chat-text border rounded p-1 mb-1">
+                                        <h6 className="chat-username" onClick={() => handleClickName(msg.id)}>{msg.name} {msg.lastName}</h6>
+                                        <p className="m-0">{msg.message}</p>
+                                    </div>
                                 );
                             })}
                         </div>
@@ -78,17 +75,6 @@ export const University = () => {
                             <input type="text" className="form-control" placeholder="mensaje" value={message} onChange={handleChangeMsg}/>
                             <button onClick={handleSendMsg} className="btn btn-primary">Enviar</button>
                         </div>
-                    </div>
-
-                    <div>
-                        <h4>Usuarios Conectados</h4>
-                        <ul className="p-0">
-                            {usersOnline.map((uo) => {
-                                return (
-                                    <li>{uo.name} {uo.lastName}</li>
-                                );
-                            })}
-                        </ul>
                     </div>
                 </div>
             </section>
