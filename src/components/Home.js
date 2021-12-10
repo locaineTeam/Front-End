f9757450cd479f860c595c2582ff7ebb9167040d
IJuanchoG <ijuankhoxd@gmail.com>
2021-12-10 18:10:26 -0500

    Con imágenes el Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,6 +9,14 @@ export const Home = () => {
     const history = useHistory();
     const [universitys, setUniversitys] = useState([]) ;
     
+    const cache = {};
+
+    const images = Object.entries(cache).map(module => module[1].default);
+
+    function importAll(r) {
+        r.keys().forEach((key) => (cache[key] = r(key)));
+    }   
+
     const handleUniversity = (name) => {
         
         history.push("/university/"+name);
@@ -29,26 +37,37 @@ export const Home = () => {
              
     }, []);
 
+    importAll(require.context("../Assets/university_logo", false, /.(png|jpe?g|svg)$/));
+
     return(
         <>
-            <HeaderContent />
+            <HeaderContent />            
             <section className="homepage py-3">
                 <div className="subHomepage mx-auto p-2 rounded">
                     <h2>Universidades</h2>
                     <table className="table table-hover table-borderless table-striped" id="statTable">
                         <tbody>
-                            {universitys.map((uni) => {
-                                return(
-                                    <tr>
-                                        <td>
-                                            <h5>{uni.name}</h5>                
-                                            <button className="btn btn-primary" onClick={() => handleUniversity(uni.name)}>Entrar</button>
-                                        </td>
-                                    </tr>
-                                );
+                            {Object.entries(cache).map(module => {                                
+                                const image = module[1].default;
+                                const name = module[0].replace("./","");
+                                const min = 1;
+                                const max = 200;
+                                const hombres = Math.ceil(min + Math.random() * (max - min));
+                                const mujeres = Math.ceil(min + Math.random() * (max - min));
+                                return (
+                                <tr>
+                                    <td>
+                                        <h5>{name.replace('.png','')}</h5>  
+                                        <p>Hombres: {hombres}  Mujeres: {mujeres}</p>
+                                        <img style={{float: 'left',width: 90, height: 70, margin: 'auto', display: 'block'}} src={image} />  
+                                        <button className="btn btn-primary" onClick={() => handleUniversity(name.replace('.png',''))}>Entrar</button>
+                                    </td>
+                                </tr>
+                                )
                             })}
                         </tbody>
                     </table>
+                    
                 </div>
             </section>
         </>
